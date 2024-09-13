50fb8fb91f94b12f86ad696e7a7d395f0d098ca6
Ankit <iankitraj18@gmail.com>
2024-09-13 18:07:13 +0530

    Notes

---
+++ DSA Series/Notes.cpp
@@ -0,0 +1,20 @@
+// Variable
+// Containers to store Data
+
+// Rule:
+    // 1.For any name must be start as - (Underscore)
+    // 2.For any name must be end as - (Underscore).
+    // 3.Any name must be contain only english characteers a-z A-Z and 
+    //   underscore _ and dot .
+
+    // Data Types
+
+        // int - 4bytes
+        // float - 4bytes
+        // char - 1byte
+        // bool - 1byte
+        // string - 2bytes
+        // list - 8bytes
+        // dict - 8bytes
+        // set - 8bytes
+        
