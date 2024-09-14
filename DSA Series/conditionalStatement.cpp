11701239fee3f96c717f2ad4803a9461eb2ac43e
Ankit <iankitraj18@gmail.com>
2024-09-14 13:15:12 +0530

    Ankit

--- DSA Series/conditionalStatement.cpp
+++ DSA Series/conditionalStatement.cpp
@@ -1,18 +1,43 @@
-#include <iostream>  // Include the iostream library for input and output 
-using namespace std; // Use the standard namespace to avoid using "std::" prefix
+// #include <iostream>  // Include the iostream library for input and output 
+// using namespace std; // Use the standard namespace to avoid using "std::" prefix
 
+// int main() {
+//     int a = -100; // Initialize the integer variable 'a' with the value -100
+
+//     // Check if 'a' is less than or equal to 0
+//     if ((a<=0))
+//     {
+//         // If the condition is true then print this message
+//         cout << "a is positive" << endl;
+//     } else {
+
+//         // If the condition is false then print this message
+//         cout << "a is negative" << endl;
+//     }
+//     return 0; // Return 0 to indicate successful program execution
+// }
+
+// 
+
+#include <iostream> //Include the iostream library for input and output
+using namespace std; //Use the standard namespace to avoid using "Std::" prefix
+
+//Passing Main method
 int main() {
-    int a = -100; // Initialize the integer variable 'a' with the value -100
+    int a; //Initilize the integer variable name 'a'
 
-    // Check if 'a' is less than or equal to 0
-    if ((a<=0))
+    // Prompt the user to enter a number
+    cout <<"Enter Any Number:";
+    cin >> a; // Read the input from the user and store in variable 'a'
+
+    // Check if the number is even by using modulus operator
+    if (a%2 == 0) // If the remainder when 'a' is divided by 2 is zero, it is an even numbe
     {
-        // If the condition is true then print this message
-        cout << "a is positive" << endl;
-    } else {
+        cout <<"Even Number\n";   // Output that the number is even
 
-        // If the condition is false then print this message
-        cout << "a is negative" << endl;
+    } else {  // If the condition is false, meaning the number is odd
+        cout <<"Odd Number\n";  // Output that the number is odd 
     }
+    
     return 0; // Return 0 to indicate successful program execution
 }
