1c38e6a29e4c0090738d3d5ba43e0fc33ec43ad5
Ankit <iankitraj18@gmail.com>
2024-09-14 13:16:35 +0530

    dfghj

--- DSA Series/conditionalStatement.cpp
+++ DSA Series/conditionalStatement.cpp
@@ -1,23 +1,23 @@
-// #include <iostream>  // Include the iostream library for input and output 
-// using namespace std; // Use the standard namespace to avoid using "std::" prefix
+#include <iostream>  // Include the iostream library for input and output 
+using namespace std; // Use the standard namespace to avoid using "std::" prefix
 
-// int main() {
-//     int a = -100; // Initialize the integer variable 'a' with the value -100
+int main() {
+    int a = -100; // Initialize the integer variable 'a' with the value -100
 
-//     // Check if 'a' is less than or equal to 0
-//     if ((a<=0))
-//     {
-//         // If the condition is true then print this message
-//         cout << "a is positive" << endl;
-//     } else {
+    // Check if 'a' is less than or equal to 0
+    if ((a<=0))
+    {
+        // If the condition is true then print this message
+        cout << "a is positive" << endl;
+    } else {
 
-//         // If the condition is false then print this message
-//         cout << "a is negative" << endl;
-//     }
-//     return 0; // Return 0 to indicate successful program execution
-// }
+        // If the condition is false then print this message
+        cout << "a is negative" << endl;
+    }
+    return 0; // Return 0 to indicate successful program execution
+}
 
-// 
+
 
 #include <iostream> //Include the iostream library for input and output
 using namespace std; //Use the standard namespace to avoid using "Std::" prefix
@@ -38,6 +38,6 @@ int main() {
     } else {  // If the condition is false, meaning the number is odd
         cout <<"Odd Number\n";  // Output that the number is odd 
     }
-    
+
     return 0; // Return 0 to indicate successful program execution
 }
