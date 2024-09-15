80be5ef3c7fa9f31548b91bb6807427f5d70e334
Ankit <iankitraj18@gmail.com>
2024-09-15 14:16:26 +0530

    Text

--- DSA Series/one.cpp
+++ DSA Series/one.cpp
@@ -1,3 +1,23 @@
+//How does code run?
+    // (i)Source Code -->Compiler-->Executable File
+
+//How to write main method
+
+//int main() {
+    //Wrirte here any Statement
+//}
+
+//Boilerplate Code
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     //Write here any statement
+//     return 0;
+// }
+
+
 #include<iostream>
 using namespace std;
 int main() {
